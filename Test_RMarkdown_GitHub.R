ce1f8817a69da3b5fe7696a56d685a8916ad7abe
SONIA MARIE VARGAS <SONIAMARIEVARGAS@GMAIL.COM>
2019-03-06 06:24:57 -0800

    Commit from RMarkdown 8

---
+++ Test_RMarkdown_GitHub.R
@@ -0,0 +1,19 @@
+#' ---
+#' title: "Test of RMarkdown with GitHub"
+#' author: "Sonia Vargas"
+#' date: "March 6, 2019"
+#' output: 
+#' html_document: 
+#'    #keep_md: yes
+#' ---
+#' to push to github, do:
+#' git add -A
+#' git commit -m "commit comment"
+#' git push
+
+#+ r setup, include =  FALSE
+#'insert your brilliant WORKING CODE HERE
+x= "An english sentence"
+print(x)
+
+
